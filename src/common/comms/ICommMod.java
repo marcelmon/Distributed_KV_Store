54bff0636ff2108ed86523938c0671c4bbb3704d
Tom Kingsford <tom@thomaskingsford.com>
2018-01-27 15:46:29 -0500

    Fixed communications logic. Clients now always get a response when they send a message.

--- src/common/comms/ICommMod.java
+++ src/common/comms/ICommMod.java
@@ -1,15 +1,17 @@
 package common.comms;
 
 import common.messages.*;
+import java.io.*;
 
 public interface ICommMod {
 	// Server
 	public void StartServer(int port) throws Exception;
 	public void StopServer() throws Exception;
 	public void SetListener(ICommListener listener);
+	public void SendMessage(KVMessage msg, OutputStream client) throws Exception;
 	
 	// Client
 	public void Connect(String ip, int port) throws Exception;
-	public void Disconnect();	
-	public void SendMessage(KVMessage msg) throws Exception;
+	public void Disconnect();
+	public KVMessage SendMessage(KVMessage msg) throws KVMessage.StreamTimeoutException, Exception;
 }
