fb80749da2bd7da255947f78258673577de7b353
Tom Kingsford <tom@thomaskingsford.com>
2018-01-28 18:42:42 -0500

    Broke up tests into AllTests and UnitTests. CI only runs UnitTests, except for on master where it runs AllTests

---
+++ src/testing/UnitTests.java
@@ -0,0 +1,40 @@
+package testing;
+
+import java.io.IOException;
+
+import org.apache.log4j.Level;
+
+import app_kvServer.KVServer;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import logger.LogSetup;
+
+
+public class UnitTests {
+
+	static {
+		try {
+			new LogSetup("logs/testing/test.log", Level.ERROR);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static Test suite() {
+		TestSuite clientSuite = new TestSuite("Storage Server Unit Test-Suite");
+
+		clientSuite.addTestSuite(TLVMessageTest.class);
+		clientSuite.addTestSuite(SocketTest.class);
+		clientSuite.addTestSuite(CommModTest.class);
+		
+		clientSuite.addTestSuite(LockManagerTest.class);
+		clientSuite.addTestSuite(KVDBTests.class);
+		clientSuite.addTestSuite(CacheTests.class);
+		
+		clientSuite.addTestSuite(StoreServerTests.class);
+		
+		return clientSuite;
+	}
+	
+}
