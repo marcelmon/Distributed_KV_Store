e983e1ee3b410791ef768e2764e30eb2f3ee7dd8
Tom Kingsford <tom@thomaskingsford.com>
2018-03-11 19:19:12 -0400

    Added an AdminConsole - similar to ECSConsole

--- src/app_kvECS/ISSHLauncher.java
+++ src/app_kvECS/ISSHLauncher.java
@@ -2,5 +2,6 @@ package app_kvECS;
 
 public interface ISSHLauncher {
     // lauch a SSH connection
-    public boolean launchSSH(String hostname, int port) throws Exception;
+    public boolean launchSSH(String name, String hostname, int port, int cachesize, String cachestrategy) throws Exception;
+    public void killall();
 }
--- src/app_kvECS/SSHLauncher.java
+++ src/app_kvECS/SSHLauncher.java
@@ -1,21 +1,63 @@
 package app_kvECS;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class SSHLauncher implements ISSHLauncher {
+	protected List<Process> processes = new ArrayList<Process>();
+	
     // lauch a SSH connection
-    public boolean launchSSH(String hostname, int port) throws Exception {
+    public boolean launchSSH(String name, String hostname, int port, int cachesize, String cachestrategy) throws Exception {
         Process proc;
         // String script = "SSHScript.sh";
         
         Runtime run = Runtime.getRuntime();
         try {
-            proc = run.exec("ssh -n " + hostname + " nohup java -jar ../../m2-server.jar " + port + " ERROR &");
+        	// From SSH command:
+        	String cmd = 
+        			"ssh -n " + hostname + " " +
+        			"java -jar m2-server.jar " + 
+        			name + " " + 
+        			port + " " +
+        			"127.0.0.1 2181 " + // TODO zookeeper info should be managed better
+        			cachesize + " " +
+        			cachestrategy + 
+        			//" & echo \\$!\"";
+				" &";
+        	System.out.println(cmd);
+        	
+        	// Execute command:
+            proc = run.exec(cmd);
+            processes.add(proc);
+            
+            // Get the output from SSH:
+	    // Thread.sleep(1000);
+            proc.waitFor(1000, TimeUnit.MILLISECONDS);
+            InputStreamReader isr = new InputStreamReader(proc.getInputStream());
+            BufferedReader br = new BufferedReader(isr);
+            int c = br.read();
+            while (br.ready()) {
+                System.out.print((char)c);
+		c = br.read();
+            }
         } catch (IOException e) {
             proc = null;
             throw new Exception(e.getMessage());
         }
 
         return true;
+    }
+    
+    public void killall() {
+    	//FIXME implement properly
+    	for (Process p : processes) {
+    		p.destroy();    		
+    	}
+    	processes.clear();
     }
 }
